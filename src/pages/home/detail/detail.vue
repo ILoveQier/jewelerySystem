3b8589c907e21f26296f029716d5bc15b3a0698c
mydeer <597584896@qq.com>
2019-04-15 08:45:08 +0800

    detail

---
+++ src/pages/home/detail/detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{shop.shopname}}
+  </div>
+</template>
+<script>
+export default {
+  props:['shop'],
+  onLoad(ops) {
+    this.shop = JSON.parse(this.$getRoute().shop)
+  }
+}
+</script> 
+<style lang="less">
+</style>
+
---
+++ src/pages/home/detail/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Detail from './detail.vue'
+
+const detail = new Vue(Detail)
+
+detail.$mount() // 挂在当前页面
